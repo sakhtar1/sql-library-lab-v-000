ad49dfdb081da655234c2397f90e7a987fa62e53
Sharmina Akhtar <sakht318@gmail.com>
2018-02-14 16:44:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,5 +42,5 @@ def select_character_names_and_number_of_books_they_are_in
   INNER JOIN books
   ON character_books.book_id = books.id
   Group by characters.name
-  WHERE books.id = characters.id;"
+  ORDER by books.title;"
 end
