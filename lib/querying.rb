4149cc5cfa018b0fedca91081a96a77ed38a2f5a
Sharmina Akhtar <sakht318@gmail.com>
2018-02-14 15:01:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,8 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters
+  WHERE COUNT(characters.motto) > 10;"
 end
 
 
